95f7e30e5395d50e1ca3a3bf91eed8ee93e39322
Jose <jsotomoriano@esteveterradas.cat>
2019-03-20 10:21:23 +0100

    Añadir codi en la clase Nuevo

--- ClassesAbstractes/src/Practica/Nuevo.java
+++ ClassesAbstractes/src/Practica/Nuevo.java
@@ -1,5 +1,10 @@
 package Practica;
-
+import java.util.Scanner;
 public class Nuevo {
+public static void main(String[] args) {
+	String nom = null ;
+	System.out.println("Introdueix nom: "+ nom);
+	
+}
 
 }
